8c764ed9b0c617fe2cacb17bdf29c4997f7414a8
Muhammad Balyan <mmbalyann@gmail.com>
2020-09-23 00:19:39 +0700

    change number of looping

--- app/Database/Seeds/PeopleSeeder.php
+++ app/Database/Seeds/PeopleSeeder.php
@@ -9,7 +9,7 @@ class PeopleSeeder extends \CodeIgniter\Database\Seeder
     public function run()
     {
         $faker = \Faker\Factory::create('id_ID');
-        for ($i = 0; $i < 300; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             $name = $faker->name;
             $slug = url_title($name, '-', true);
 
